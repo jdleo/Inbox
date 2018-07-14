d5f8be9269d7e010830a9e103484cd4a239e1525
John Leonardo <jdleonardo5@icloud.com>
2018-07-13 20:13:49 -0700

    test for contract deployment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,18 +2,24 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async () => {
   //get a list of all ETH accounts
-  web3.eth.getAccounts().then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
 
   //use one of these accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['hello world!']})
+    .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox Contract', () => {
-  it('deployed a contract', () => {
-
+  it('deploys a contract', () => {
+    //if an address exists, it's likely that the contract was deployed
+    assert.ok(inbox.options.address);
   });
 });
