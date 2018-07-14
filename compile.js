bb54dee5b16deaf559b783f084501e577ce1d16c
John Leonardo <jdleonardo5@icloud.com>
2018-07-13 19:08:02 -0700

    compile.js

---
+++ compile.js
@@ -0,0 +1,14 @@
+//bring in path and fs so that we can read .sol file
+const path = require('path');
+const fs = require('fs');
+
+//bring in solidity compiler
+const solc = require('solc');
+
+//generate path to point to Inbox.sol contract file
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+
+//read .sol source
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
